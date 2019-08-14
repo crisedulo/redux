806ac67d01b3e1dda8aa6226156b284c9b2a89a2
crisedulo <crisedulo@gmail.com>
2019-08-14 10:27:02 -0600

    Updates

---
+++ precache-manifest.6b82ad87dc1b79cff2592ba284cf019f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25dda3f17ae9078beab5c70f9f23948",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "ebe8f85a7342d2bf51c4",
+    "url": "/redux/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "a9f4438fe3b57d859ac4",
+    "url": "/redux/static/js/2.35c5f3d6.chunk.js"
+  },
+  {
+    "revision": "84893a9746dc11f4c586",
+    "url": "/redux/static/js/3.db4a45bb.chunk.js"
+  },
+  {
+    "revision": "ebe8f85a7342d2bf51c4",
+    "url": "/redux/static/js/main.24adeb13.chunk.js"
+  },
+  {
+    "revision": "49c306ab8b7907a9a522",
+    "url": "/redux/static/js/runtime~main.1dbff770.js"
+  }
+]);
